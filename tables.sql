97abdfe8a28d6d11a0fe6b29a87a878cf4e3da91
zhanf164 <hanf.27@osu.edu>
2022-07-09 22:51:30 -0400

    pushing for Briant

---
+++ tables.sql
@@ -0,0 +1,7 @@
+Create Table User (firstName, lastName, email, userName, password, optional=Group);
+
+Create Table Preferences (user, dates);
+
+Create Table reservations (user, reservations, date);
+
+Create table photos (user, photo1, photo2, photo3);
